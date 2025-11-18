1e431f919f096b5fe36cd7ad3fdf11408b55bd47
suhanidhanupur-su <suhanidhanupur@gmail.com>
2025-11-18 09:41:27 +0530

    sum

---
+++ function.py
@@ -0,0 +1,13 @@
+a = 5
+b = 10
+print("The sum of a and b is : ", a + b)
+print("it's the second time i am calling my function")
+
+for i in range(3):
+                    print("i")
+print("it's the third time i am calling my function")
+
+
+
+
+
