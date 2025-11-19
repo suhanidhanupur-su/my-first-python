6943e6ac26bf08e8e26e4fef4cb3af59d20c693d
suhanidhanupur-su <suhanidhanupur@gmail.com>
2025-11-19 14:05:23 +0530

    profile

---
+++ variable.py
@@ -0,0 +1,46 @@
+first_name = "suhani"first_name = "suhani"
+_last_name = "kumari"
+# 1_name = "palak"
+print(first_name)
+print(_last_name)
+# print(1_name)
+
+# var# = "1000"
+# print(var#)
+
+
+my_name = "Nisha"
+# print(MY_NAME)
+
+# finally = "kushi"
+# print(finally)
+
+# listt = [111, 222, 3222, 422, 522]
+# for i in listt:
+#     print(i)
+
+
+# Job k regarding form application
+
+_last_name = "kumari"
+# 1_name = "palak"
+print(first_name)
+print(_last_name)
+# print(1_name)
+
+# var# = "1000"
+# print(var#)
+
+
+my_name = "Nisha"
+# print(MY_NAME)
+
+# finally = "kushi"
+# print(finally)
+
+# listt = [111, 222, 3222, 422, 522]
+# for i in listt:
+#     print(i)
+
+
+# Job k regarding form application
