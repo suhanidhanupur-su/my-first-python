b93fd56a027090ff0b6a49c48ee368fdd98927d3
suhanidhanupur-su <suhanidhanupur@gmail.com>
2025-11-19 09:23:50 +0530

    code

---
+++ about.py
@@ -0,0 +1,33 @@
+Types:
+1. Arithmetic Operators
+2. Assignment Operators
+3. Comparison Operators
+4. Logical Operators
+5. Bitwise Operators
+6. Identity Operators
+7. Membership Operators
+
+1. Arithmetic Operators:
++ , - , * , / ,//,  % , ** 
+
++ => Addition
+- => Subtraction
+* => Multiplication
+
+/ => Division  => it gives us float value, it gives us Quotient value.
+
+// => Floor Division  => it gives us integer value, it gives us Quotient value.
+
+% => Modulus  => it gives us remainder value.
+
+** => Exponentiation  => it gives us power value.
+
+
+
+
+
+## Data Types:
+int => 10, 20, 30....
+float => 10.5, 20.5, 30.5....
+string => "Hello", "World", "Ravi"
+boolean => True, False
