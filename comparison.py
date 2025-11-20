07d876b1842e9e7535de12936563e3bfccfe367e
suhanidhanupur-su <suhanidhanupur@gmail.com>
2025-11-20 17:26:14 +0530

    first commit

---
+++ comparison.py
@@ -0,0 +1,19 @@
+# 3. Comparison Operators
+
+# => It is used to compare two values.
+# => it returns boolean value(True or False)
+
+# EX: ==, !=, >, <, >=, <=
+
+G = 10
+H = 20
+print(G > H ) 
+print( G < H) 
+
+print(G == H) 
+
+print( G != H ) 
+
+print( G <= H ) 
+
+print( G >= H ) 
