d1ae049c584c57e0ec6b5dd42957ca3d204282ba
suhanidhanupur-su <suhanidhanupur@gmail.com>
2025-11-20 17:22:31 +0530

    commit

---
+++ logic.py
@@ -0,0 +1,9 @@
+A = 10
+B = 20
+print("the output of this A < B and A == 10 is:", A < B and A == 10)
+print("the output of this A > B and A != B is:", A > B and A != B)
+
+print("the output of this A < B or A == 10 is:", A < B or A == 10)
+
+print(A < B or A > 50)
+# the output of line number 31 is: True
