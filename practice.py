a410377bbbe9039edfd8578ed04cd439ddf0743b
suhanidhanupur-su <suhanidhanupur@gmail.com>
2025-11-22 17:00:32 +0530

    string

---
+++ practice.py
@@ -0,0 +1,14 @@
+## String : -
+# => A sequence of characters/alphabets/letters
+
+
+# Letters => Characters
+# Sentence/Words => Group of characters/alphabets => String 
+
+# Example : "wilmot","Sharik", "Hello World" , 'Python Programming' , """Welcome to Python""" , '''Python is fun'''
+
+# Alphabets/Letters/Characters : a,b,c.......z , A,B,C....Z
+
+## Words/Sentences : group of characters/alphabets
+
+
